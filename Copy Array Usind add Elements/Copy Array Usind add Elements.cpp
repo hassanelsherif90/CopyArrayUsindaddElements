6fb7cb0ebc0e4dcece975728e601368b891e5da1
hassan elsherif <hassanelsherif90@gmail.com>
2023-03-25 02:28:25 +0200

    Add project files.

---
+++ Copy Array Usind add Elements/Copy Array Usind add Elements.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int RandNumber(int From, int To)
+{
+	int RandNum = rand() % (To - From + 1) + From;
+
+	return RandNum;
+}
+
+void FillArrayElements(int arr[100], int & arrlength)
+{
+	cout << "\nplease enter a number of array ? ";
+	cin >> arrlength;
+
+	for (int i = 0; i < arrlength; i++)
+	{
+		arr[i] = RandNumber(1, 100);
+	}
+}
+
+void PrintArray(int arr[100], int arrlength)
+{
+	for (int i = 0; i < arrlength; i++)
+	{
+		cout << arr[i]<< " ";
+	}
+	cout << "\n";
+}
+
+void AddArrayElements(int Number ,int arr[100], int& arrlength)
+{
+	arrlength++;
+	arr[arrlength - 1] = Number;
+}
+
+void CopyArrayUsingAddElements(int arrSource[100], int arrDestnations[100], int arrlength, int& arr2length)
+{
+	for (int i = 0; i < arrlength; i++)
+	{
+		AddArrayElements(arrSource[i], arrDestnations, arr2length);
+	}
+}
+
+int main()
+{
+	int arr[100], arrlength = 0, arr2length = 0;
+	FillArrayElements(arr, arrlength);
+	cout << "Array 1 Elements : \n";
+	PrintArray(arr, arrlength);
+
+	int arr2[100];
+	CopyArrayUsingAddElements(arr,arr2, arrlength, arr2length);
+	cout << "Array 2 Elements after copy: \n";
+	PrintArray(arr2, arr2length);
+
+	return 0;
+}
